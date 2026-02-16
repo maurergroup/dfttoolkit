aa75dc682153be5974954c885bacc2de2779e19c
Lukas Hoermann <lukashoermann2000@gmail.com>
2026-02-16 16:04:24 +0100

    fixed minor bug in vibrations

--- dfttoolkit/vibrations.py
+++ dfttoolkit/vibrations.py
@@ -42,7 +42,9 @@ class Vibrations:
         return self._vibration_coords
 
     @vibration_coords.setter
-    def vibration_coords(self, vibration_coords: list[npt.NDArray[np.float64]]) -> None:
+    def vibration_coords(
+        self, vibration_coords: list[npt.NDArray[np.float64]]
+    ) -> None:
         self._vibration_coords = vibration_coords
 
     @property
@@ -50,7 +52,9 @@ class Vibrations:
         return self._vibration_forces
 
     @vibration_forces.setter
-    def vibration_forces(self, vibration_forces: list[npt.NDArray[np.float64]]) -> None:
+    def vibration_forces(
+        self, vibration_forces: list[npt.NDArray[np.float64]]
+    ) -> None:
         self._vibration_forces = vibration_forces
 
     @property
@@ -156,7 +160,9 @@ class Vibrations:
         N = len(self) * 3  # pyright:ignore
         H = np.zeros([N, N])
 
-        if not np.allclose(self.coords, self.vibration_coords[0]):  # pyright:ignore
+        if not np.allclose(
+            self.coords, self.vibration_coords[0]
+        ):  # pyright:ignore
             raise ValueError(
                 "The first entry in vibration_coords must be identical to the "
                 "undispaced geometry."
@@ -167,7 +173,9 @@ class Vibrations:
 
         n_forces = np.zeros(N, np.int64)
 
-        for c, F in zip(self.vibration_coords, self.vibration_forces, strict=False):
+        for c, F in zip(
+            self.vibration_coords, self.vibration_forces, strict=False
+        ):
             dF = F.flatten() - F_0
             dx = c.flatten() - coords_0
             ind = np.argmax(np.abs(dx))
@@ -181,7 +189,9 @@ class Vibrations:
 
         for row in range(H.shape[0]):
             if n_forces[row] > 0:
-                H[row, :] /= n_forces[row]  # prevent div by zero for unknown forces
+                H[row, :] /= n_forces[
+                    row
+                ]  # prevent div by zero for unknown forces
 
         if set_constrained_atoms_zero:
             constrained = self.constrain_relax.flatten()  # pyright:ignore
@@ -219,7 +229,9 @@ class Vibrations:
         constrained_inds = [i for i, c in enumerate(constrain) if c]
         constrained_inds = np.array(constrained_inds)
 
-        unconstrained_inds = np.array(list(set(all_inds) - set(constrained_inds)))
+        unconstrained_inds = np.array(
+            list(set(all_inds) - set(constrained_inds))
+        )
 
         for i in unconstrained_inds:
             for j in unconstrained_inds:
@@ -266,7 +278,9 @@ class Vibrations:
             hessian = copy.deepcopy(self.hessian)
 
         if not hasattr(self, "hessian") or hessian is None:
-            raise ValueError("Hessian must be given to calculate the Eigenvalues!")
+            raise ValueError(
+                "Hessian must be given to calculate the Eigenvalues!"
+            )
 
         M = 1 / self.get_mass_tensor()
 
@@ -294,7 +308,9 @@ class Vibrations:
 
         # Convert eigenvector to Cartesian coordinates
         if eigenvectors_to_cartesian:
-            m = np.tile(np.sqrt(self.get_atomic_masses()), (3, 1)).T  # pyright:ignore
+            m = np.tile(
+                np.sqrt(self.get_atomic_masses()), (3, 1)
+            ).T  # pyright:ignore
 
             for index in range(len(eigenvectors)):
                 eigenvectors[index] /= m
@@ -323,7 +339,8 @@ class Vibrations:
         omega = np.sign(omega2) * np.sqrt(np.abs(omega2))  # pyright:ignore
 
         conversion = np.sqrt(
-            (units.EV_IN_JOULE) / (units.ATOMIC_MASS_IN_KG * units.ANGSTROM_IN_METER**2)
+            (units.EV_IN_JOULE)
+            / (units.ATOMIC_MASS_IN_KG * units.ANGSTROM_IN_METER**2)
         )
         return omega * conversion
 
@@ -350,7 +367,9 @@ class Vibrations:
         self, omega2: npt.NDArray[np.float64] | None = None
     ) -> npt.NDArray[np.float64]:
         omega_SI = self.get_eigenvalues_in_Hz(omega2=omega2)
-        return omega_SI * units.PLANCK_CONSTANT / (2 * np.pi) / units.JOULE_IN_EV
+        return (
+            omega_SI * units.PLANCK_CONSTANT / (2 * np.pi) / units.JOULE_IN_EV
+        )
 
     def get_thermally_displaced_geometry(
         self, temperature: np.float64, classical: bool = True
@@ -396,25 +415,23 @@ class Vibrations:
                 var_qi = kB * temperature / (omega**2)
             else:
                 hbar = 1.054571817e-34
-                # var_qi = (
-                #     (hbar / (2 * omega))
-                #     * np.cosh(hbar * omega / (2 * kB * temperature))
-                #     / np.sinh(hbar * omega / (2 * kB * temperature))
-                # )
-                var_qi = (hbar / (2 * omega)) / np.tanh(
-                    hbar * omega / (2 * kB * temperature)
+                var_qi = (hbar / (2 * omega)) * (
+                    0.5 + 1.0 / (np.exp(hbar * omega / (kB * temperature)) - 1)
                 )
 
             # Convert to atomic units
             # var_qi (kg m²) -> var_qi (u A²)
             var_qi *= 1e20 / units.ATOMIC_MASS_IN_KG
 
-            amp = np.random.Generator(0.0, np.sqrt(var_qi))  # pyright:ignore
+            # amp = np.random.Generator(0.0, np.sqrt(var_qi))  # pyright:ignore
+            amp = np.random.default_rng().normal(0.0, np.sqrt(var_qi))
             print(amp)
             displacement += amp * self.eigenvectors[i]
 
         # Convert from mass weighted to Cartesian coordinates
-        m = np.tile(np.sqrt(self.get_atomic_masses()), (3, 1)).T  # pyright:ignore
+        m = np.tile(
+            np.sqrt(self.get_atomic_masses()), (3, 1)
+        ).T  # pyright:ignore
 
         displacement /= m
 
@@ -429,7 +446,9 @@ class Vibrations:
         masses = self.get_mass_of_all_atoms()  # pyright:ignore
         tolerance = 0.001
 
-        primitive_cell_inverse = np.linalg.inv(self.lattice_vectors)  # pyright:ignore
+        primitive_cell_inverse = np.linalg.inv(
+            self.lattice_vectors
+        )  # pyright:ignore
 
         atom_type_index = np.array([None] * n_atoms)
         counter = 0
@@ -453,7 +472,9 @@ class Vibrations:
                     difference_in_cell_coordinates,
                 )
                 separation = pow(
-                    np.linalg.norm(projected_coordinates_atom_j - coordinates_atom_i),
+                    np.linalg.norm(
+                        projected_coordinates_atom_j - coordinates_atom_i
+                    ),
                     2,
                 )
 
